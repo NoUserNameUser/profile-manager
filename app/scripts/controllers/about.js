4b6414fc2d92102b1492dc8371a140bd1310d16a
Jackie Ye <find.jackie@yahoo.com>
2015-08-25 00:40:09 -0700

    setup

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name profileManagerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the profileManagerApp
+ */
+angular.module('profileManagerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
